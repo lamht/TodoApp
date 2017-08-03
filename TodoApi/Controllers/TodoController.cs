d292c1a4f13eedcf9de0e191ae89d677769df37f
Thanh Lam <hthanhlam@tma.com.vn>
2017-08-03 18:06:33 +0700

    new

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+        private readonly ILogger _logger;
+
+        public TodoController(TodoContext context, ILogger<TodoController> logger)
+        {
+                      
+            _context = context;
+            _logger = logger;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            _logger.LogInformation("Get list item ===========");  
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            _logger.LogInformation("Get item {ID}", id);
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }  
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            _logger.LogInformation("Create item {ID}", item.Name);
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+             _logger.LogInformation("Update item {ID}", id);
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }    
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _logger.LogInformation("Delete item {ID}", id);
+            var todo = _context.TodoItems.First(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        } 
+
+    }
+}
